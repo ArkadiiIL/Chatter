b3104b140cd9d82e9c0dc344f85ec4d8a6ad5c80
ArkadiiIl <odinls91@gmail.com>
2020-03-07 21:59:02 +0100

    FlyWay migration and profile change added

--- src/main/java/com/example/chatter/service/UserService.java
+++ src/main/java/com/example/chatter/service/UserService.java
@@ -8,9 +8,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -32,10 +33,14 @@ public class UserService implements UserDetailsService {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
+        sendMessage(user);
+        return true;
+    }
+
+    private void sendMessage(User user) {
         String message = String.format("Hello %s! \n" +
                 "Please visit next link for activation your account: http://localhost:8080/activate/%s",user.getUsername(),user.getActivationCode());
         mailSender.send(user.getEmail(),"Activation code",message);
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -50,5 +55,42 @@ public class UserService implements UserDetailsService {
         userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(String username, Map<String, String> form, User user) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        user.getRoles().clear();
+        for(String role : form.keySet())
+        {
+            if(roles.contains(role))
+            {
+                user.getRoles().add(Role.valueOf(role));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+        boolean isChanged = (email != null && !email.equals(userEmail) || userEmail != null && !userEmail.equals(email));
+        if(isChanged)
+        {
+            if(!StringUtils.isEmpty(email))
+            {
+                user.setEmail(email);
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+        if(!StringUtils.isEmpty(password))
+        {
+            user.setPassword(password);
+        }
+        userRepo.save(user);
+        if(isChanged) sendMessage(user);
     }
 }
