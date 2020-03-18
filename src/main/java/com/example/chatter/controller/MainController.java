5492b6589dea0101aa2be541aa4b0e184c31cc79
ArkadiiIl <odinls91@gmail.com>
2020-03-18 17:21:02 +0100

    pages added

--- src/main/java/com/example/chatter/controller/MainController.java
+++ src/main/java/com/example/chatter/controller/MainController.java
@@ -5,6 +5,10 @@ import com.example.chatter.domain.User;
 import com.example.chatter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,13 +40,20 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model)
+    public String main(@RequestParam(required = false, defaultValue = "") String filter,
+                       Model model,
+                       @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
+    )
     {
-        Iterable<Message> messages = messageRepo.findAll();
+        Page<Message> page;
         if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
+            page = messageRepo.findByTag(filter,pageable);
         }
-        model.addAttribute("messages",messages);
+        else page = messageRepo.findAll(pageable);
+
+        model.addAttribute("page", page);
+        model.addAttribute("url","/main");
+        model.addAttribute("messages",page);
         model.addAttribute("filter",filter);
 
         return "main";
@@ -92,15 +103,18 @@ public class MainController {
             @AuthenticationPrincipal User currentUser,
             @PathVariable User user,
             Model model,
-            @RequestParam(required = false) Message message
+            @RequestParam(required = false) Message message,
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
     )
     {
-        Set<Message> messages = user.getMessages();
+        Page<Message> page = messageRepo.findByAuthor(user,pageable);
         model.addAttribute("userChannel",user);
         model.addAttribute("subscriptionsCount",user.getSubscriptions().size());
         model.addAttribute("subscribersCount",user.getSubscribers().size());
         model.addAttribute("isSubscriber",user.getSubscribers().contains(currentUser));
-        model.addAttribute("messages", messages);
+        model.addAttribute("page", page);
+        model.addAttribute("messages", page);
+        model.addAttribute("url","/user-messages/" + user.getId());
         model.addAttribute("message",message);
         model.addAttribute("isCurrentUser",currentUser.equals(user));
         return "userMessages";
