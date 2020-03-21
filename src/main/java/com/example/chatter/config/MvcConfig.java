d23eb1c6b7c515735c0e0fc62af2931b0aae5136
ArkadiiIl <odinls91@gmail.com>
2020-03-22 00:00:32 +0100

    Turbolinks added

--- src/main/java/com/example/chatter/config/MvcConfig.java
+++ src/main/java/com/example/chatter/config/MvcConfig.java
@@ -1,11 +1,12 @@
 package com.example.chatter.config;
 
+import com.example.chatter.util.RedirectInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,5 +31,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
                     .addResourceLocations("file:/" + uploadPath + "/");
             registry.addResourceHandler("/static/**")
                     .addResourceLocations("classpath:/static/");
+        }
+
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(new RedirectInterceptor());
         }
     }
